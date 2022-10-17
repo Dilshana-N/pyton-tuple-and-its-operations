5b5e83366fece1023a848e02eb326dcd484e1ad1
Dilshana-N <dilshanan2001@gmail.com>
2022-10-17 13:07:26 +0530

    python tuple

---
+++ accesstuple.py
@@ -0,0 +1,23 @@
+# print specified item in tuple
+flowers=("sunflower","jasmin","lilly","lotus")
+print(flowers[2])
+
+# -ve indexing
+flowers=("sunflower","jasmin","lilly","lotus")
+print(flowers[-4])
+
+# range of indexes
+flowers=("sunflower","jasmin","lilly","lotus")
+print(flowers[1:4])
+
+# example 2
+flowers=("sunflower","jasmin","lilly","lotus")
+print(flowers[:4])
+
+#example 3
+flowers=("sunflower","jasmin","lilly","lotus")
+print(flowers[2:])
+
+# negative indexes
+flowers=("sunflower","jasmin","lilly","lotus")
+print(flowers[-4:-1])
---
+++ tuple.py
@@ -0,0 +1,35 @@
+cities=("calicut","mumbai","delhi","kolkata")
+print(cities)
+
+# example 2
+tuple=("q",2,3,True)
+print(tuple)
+
+# allow duplicates
+tuple=("q",2,3,True,3,3,3)
+print(tuple)
+
+# tuple length
+tuple=(1,2,4,"t","y","t","Fg")
+print(len(tuple))
+
+#type
+flower=("sunflower","hibisc","jasmin",1,9,6,8,)
+print(type(flower))
+
+# with "," its tuple without its not
+flower=("sunflower",)
+print(type(flower))
+
+# Tuple data types
+tuple1=("apple","banana","mango")
+tuple2=(1,6,7,8,9,0)
+tuple3=(True,False,True,True)
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
+
+# tuple constructor
+# tuple1=tuple(("apple","banana","mango"))
+# print(tuple1)
